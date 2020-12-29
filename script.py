b7c964e8188dc0146931e0b679b1388c7fda61aa
Raneem Tarfa <65374231+rtarfa@users.noreply.github.com>
2020-12-28 21:11:49 -0500

    Add files via upload

--- script.py
+++ script.py
@@ -4,11 +4,16 @@ from playlist import playlist
 import library
 import pickle
 from audioplayer import AudioPlayer as ap
+import ytdownloader
+
+#MAIN TODO: remove text when downloading, don't download if already in library, upload to github, fix playlist quit, download song when add to playlist
+#get file to delete after quit
 
 #u can import itunes library??
-
+#add error exceptions
 #while input is not e (quit); loop
 
+#note: don't want it to download a song everytime you play one (so check first if in playlists)
 
 def menu():
     print("Welcome to Muse! What would you like to do?\n")
@@ -16,7 +21,8 @@ def menu():
           "b. Add a song to an existing playlist\n"
           "c. View song library\n"
           "d. Play a song\n"
-          "e. Quit")
+          "e. Play a playlist\n"
+          "f. Quit")
 
 loop = 1
 arg = 0
@@ -26,9 +32,10 @@ while loop == 1:
     arg= input()
 
     if arg=='a':
-        titl=input("Playlist title:")
+        titl=input("Playlist title: ")
         np=playlist(titl)
         library.playlists.append(np)
+        print("Playlist added!")
 
     elif arg=='b':
         option='a'
@@ -41,14 +48,15 @@ while loop == 1:
             st= input("Song title: ")
             ar= input("Artist: ")
 
-            # for pl in library.playlists:
-            #     if playlist==pl.title:
-            #         playlist=pl
+            for pl in library.playlists:
+                if playlist==pl.title:
+                    playlist=pl
 
-            # pl.add(st,ar)
-            f= open("library.py","a") #fix this part
-            f.write(playlist.lower()+".add("+st+","+ar+")") #not working
-            f.close()
+            pl.add(st,ar)
+            ytdownloader.download(st,ar) #get rid of printed text when downloads
+            # f= open("library.py","a") #fix this part
+            # f.write(playlist.lower()+".add("+st+","+ar+")") #not working
+            # f.close()
 
             #remove last line w library list then
             #write neceaary statements to end of file, inluding library of playlists
@@ -68,31 +76,72 @@ while loop == 1:
             print("\n")
             playl = input("Playlist to view: ")
             for pl in library.playlists:
-                if pl.title==playl:
-                    pl.print()
+                if pl.title == playl:
+                    if len(pl.your_playlist)==0:
+                        print("--Empty--")
+                    else:
+                        pl.print()
             print("\n")
             option=input("What would you like to do? \n"
                   "a. View another playlist \n" #include option to delete from this playlist
                   "b. Return to main menu\n")
 
     elif arg=='d': #display playlist/available songs here
+        # print("Playlists:")
+        # for pl in library.playlists:
+        #     print(pl.title)
+        # print("\n")
+        # print("a. View all songs")
         song= input("Song title: ")
         artist= input("Artist: ")
+        ytdownloader.download(song, artist)
         #if not in library, download from youtube here
         curr_song = ap(song.lower()+".mp3")
         command=None
         #include artist in filename (like "better_khalid.mp3" program it to save like this when download from youtube)
         while command!='q':
-            command= input("Press a to play, p to pause, r to resume and q to quit.\n")
+            command= input("Press a to play, p to pause, r to resume and q to quit.\n") #have a restart option?
             if command== 'a':
-                curr_song.play()
+                curr_song.play() #loop=True
             elif command== 'p':
                 curr_song.pause()
             elif command== 'r':
                 curr_song.resume()
         curr_song.stop()
-
-    elif arg=='e':
+    elif arg == 'e':  #TODO: fix loop so that quit option works
+        option = 'a'
+        quit= False
+        while option != 'b':
+            print("Which playlist?")
+            print("Library: ")
+            for pl in library.playlists:
+                print(pl.title)
+            playlist = input()
+            #print songs here
+            # q=True
+            for pl in library.playlists:
+                if playlist == pl.title:
+                    for song in pl.your_playlist:
+                        curr_song = ap(song.title.lower() + ".mp3")
+                        print("Current song: "+ song.title + " by "+song.artist)
+                        command=None
+                        while command != 's':
+                            command = input("Press a to play, p to pause, r to resume, s to skip, and q to quit.\n")
+                            if command == 'a':
+                                curr_song.play()
+                            elif command == 'p':
+                                curr_song.pause()
+                            elif command == 'r':
+                                curr_song.resume()
+                            # elif command == 'q':
+                            #     q=True
+                        #wait until song is done
+                        curr_song.stop()
+                        curr_song.close()
+            option = input("What would you like to do? \n" #note: make sure this shows up after song finishes playing too
+                           "a. Play another playlist \n"
+                           "b. Return to main menu\n")
+    elif arg=='f':
         loop=0
 
 
---
+++ ytdownloader.py
@@ -0,0 +1,24 @@
+from __future__ import unicode_literals
+import youtube_dl
+from youtubesearchpython import SearchVideos
+
+def download(title,artist):
+    search = SearchVideos(title+" "+artist, offset = 1, mode = "list", max_results = 1)
+    url= (search.result())[0][2]
+    print(url)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': title+'.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
+#download("derniere danse", "indila")
