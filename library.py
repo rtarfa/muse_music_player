32de99ddb757da49c6d14d83176a8f8ef2a2c08e
Raneem Tarfa <65374231+rtarfa@users.noreply.github.com>
2020-12-31 11:26:27 -0500

    Update library.py

--- library.py
+++ library.py
@@ -1,25 +1,15 @@
 from song import song
 from playlist import playlist
 
-#ex1=song("better","khalid","better.jpg") #add .mp3 to end when download from youtube
-# print(ex1.title)
 
-#example
+
+#example of how to add songs to playlists manually
+
 chill=playlist("Chill")
 chill.add("Better","Khalid")
 chill.add("No Has Podido Olvidarme","Cami Moreno ")
 
-playlists=[chill]
+playlists=[chill] #script.py references this list and adds to it
 
 #pl.delete("aicha","cheb khaled")
 #pl.print()
-
-
-#lib = open("library.pkl",'rb')
-            # library=pickle.load(lib)
-            # print(library)
-            # fh.close()
-            # library.playlists.append(pl)
-            # lib= open("library.pkl",'wb')
-            # pickle.dump(library.playlists,lib)
-            # lib.close()arab.add(bom bom,mohammad ramadan)arab.add(bom bom,mohammad ramadan)arab.add(bom bom,mohammad ramadan)arab.add(bom bom,mohammad ramadan)arab.add(bombom,mr)
