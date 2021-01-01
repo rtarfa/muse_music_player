c817d342e8517aba4b49b934057ef25442e03bdf
Raneem Tarfa <65374231+rtarfa@users.noreply.github.com>
2020-12-31 20:22:53 -0500

    Update library.py

--- library.py
+++ library.py
@@ -1,14 +1,4 @@
 from song import song
 from playlist import playlist
 
-Chill=playlist("Chill")
-Chill.add("No Has Podido Olvidarme","Cami Moreno")
-Chill.add("Dile","Cami Moreno")
-Chill.add("Magnolia","Keshi")
-Chill.add("The Sound","The 1975")
-Chill.add("Better","Khalid")
-
-playlists=[Chill]
-
-
-Chill.add("The Reaper","Keshi")
+playlists=[]
